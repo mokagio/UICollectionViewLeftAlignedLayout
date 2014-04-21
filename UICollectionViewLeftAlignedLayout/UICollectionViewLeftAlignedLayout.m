78513ddf5ceacbcd140769d32aa5671bbeae4627
Gio <giovanni.lodi42@gmail.com>
2014-04-21 17:26:16 +0100

    Fixed issue with very first item not being aligned + refactor

--- UICollectionViewLeftAlignedLayout/UICollectionViewLeftAlignedLayout.m
+++ UICollectionViewLeftAlignedLayout/UICollectionViewLeftAlignedLayout.m
@@ -20,6 +20,25 @@
 
 #import "UICollectionViewLeftAlignedLayout.h"
 
+@interface UICollectionViewLayoutAttributes (LeftAligned)
+
+- (void)leftAlignFrame;
+
+@end
+
+@implementation UICollectionViewLayoutAttributes (LeftAligned)
+
+- (void)leftAlignFrame
+{
+    CGRect frame = self.frame;
+    frame.origin.x = 0;
+    self.frame = frame;
+}
+
+@end
+
+#pragma mark -
+
 @implementation UICollectionViewLeftAlignedLayout
 
 #pragma mark - UICollectionViewLayout
@@ -36,31 +55,30 @@
 }
 
 - (UICollectionViewLayoutAttributes *)layoutAttributesForItemAtIndexPath:(NSIndexPath *)indexPath {
-    UICollectionViewLayoutAttributes* currentItemAttributes =
-    [super layoutAttributesForItemAtIndexPath:indexPath];
-    
-    // first item in the section
-    if (indexPath.item == 0) return currentItemAttributes;
-    
+    UICollectionViewLayoutAttributes* currentItemAttributes = [super layoutAttributesForItemAtIndexPath:indexPath];
+
+    BOOL isFirstItemInSection = indexPath.item == 0;
+
+    if (isFirstItemInSection) {
+        [currentItemAttributes leftAlignFrame];
+        return currentItemAttributes;
+    }
+
     NSIndexPath* previousIndexPath = [NSIndexPath indexPathForItem:indexPath.item-1 inSection:indexPath.section];
     CGRect previousFrame = [self layoutAttributesForItemAtIndexPath:previousIndexPath].frame;
     CGFloat previousFrameRightPoint = previousFrame.origin.x + previousFrame.size.width;
-    
-    // if current item is the first item on the line
-    // the approach here is to take the current frame, left align it to the edge of the view
-    // then stretch it the width of the collection view, if it intersects with the previous frame then that means it
-    // is on the same line, otherwise it is on it's own new line
     CGRect currentFrame = currentItemAttributes.frame;
     CGRect strecthedCurrentFrame = CGRectMake(0,
                                               currentFrame.origin.y,
                                               self.collectionView.frame.size.width,
                                               currentFrame.size.height);
+    // if the current frame, once left aligned to the left and stretched to the full collection view
+    // widht intersects the previous frame then they are on the same line
+    BOOL isFirstItemInRow = !CGRectIntersectsRect(previousFrame, strecthedCurrentFrame);
 
-    if (!CGRectIntersectsRect(previousFrame, strecthedCurrentFrame)) {
+    if (isFirstItemInRow) {
         // make sure the first item on a line is left aligned
-        CGRect frame = currentItemAttributes.frame;
-        frame.origin.x = 0;
-        currentItemAttributes.frame = frame;
+        [currentItemAttributes leftAlignFrame];
         return currentItemAttributes;
     }
 
