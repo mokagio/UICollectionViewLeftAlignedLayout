cccaf6c9af5689420b06898e770c6bdbb0159e72
Giovanni Lodi <giovanni.lodi42@gmail.com>
2015-06-04 06:55:37 +1000

    Update and track Pods in example project

---
+++ Example/Pods/Target Support Files/Pods-Example/Pods-Example-environment.h
@@ -0,0 +1,26 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// OpenSans
+#define COCOAPODS_POD_AVAILABLE_OpenSans
+#define COCOAPODS_VERSION_MAJOR_OpenSans 1
+#define COCOAPODS_VERSION_MINOR_OpenSans 0
+#define COCOAPODS_VERSION_PATCH_OpenSans 3
+
+// UICollectionViewLeftAlignedLayout
+#define COCOAPODS_POD_AVAILABLE_UICollectionViewLeftAlignedLayout
+#define COCOAPODS_VERSION_MAJOR_UICollectionViewLeftAlignedLayout 0
+#define COCOAPODS_VERSION_MINOR_UICollectionViewLeftAlignedLayout 0
+#define COCOAPODS_VERSION_PATCH_UICollectionViewLeftAlignedLayout 3
+
+// UIColor+FlatColors
+#define COCOAPODS_POD_AVAILABLE_UIColor_FlatColors
+#define COCOAPODS_VERSION_MAJOR_UIColor_FlatColors 0
+#define COCOAPODS_VERSION_MINOR_UIColor_FlatColors 0
+#define COCOAPODS_VERSION_PATCH_UIColor_FlatColors 2
+
