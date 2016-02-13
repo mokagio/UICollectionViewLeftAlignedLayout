d2ca35e47716d785f61432caabaf74d9decadc85
Giovanni Lodi <giovanni.lodi42@gmail.com>
2016-02-13 20:33:06 +1100

    Add script running cocoapods integration tests

---
+++ Test-Integration/test-cocoapods.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+bundle install
+
+cd CocoaPodsTest
+bundle exec pod install
+cd ..
+
+xcodebuild \
+  -workspace CocoaPodsTest/CocoaPodsTest.xcworkspace \
+  -scheme CocoaPodsTest \
+  -destination "platform=iOS Simulator,name=iPhone 6,OS=latest" \
+  build \
+	| bundle exec xcpretty
