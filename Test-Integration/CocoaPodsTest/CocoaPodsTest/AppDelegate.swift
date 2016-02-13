29842cf77557cf6ffb622f4ba61917ef8c28b454
Giovanni Lodi <giovanni.lodi42@gmail.com>
2016-02-13 20:32:54 +1100

    Add project to test CocoaPods integration

---
+++ Test-Integration/CocoaPodsTest/CocoaPodsTest/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  CocoaPodsTest
+//
+//  Created by Giovanni on 13/02/2016.
+//  Copyright © 2016 mokagio. All rights reserved.
+//
+
+import UIKit
+import UICollectionViewLeftAlignedLayout
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+}
