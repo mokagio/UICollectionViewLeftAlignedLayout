8832fc4accaa868447e14c08f51540bc6a486509
Gio Lodi <gio@mokacoding.com>
2023-01-18 10:38:39 +1100

    Update copyright dates

--- UICollectionViewLeftAlignedLayout/include/UICollectionViewLeftAlignedLayout.h
+++ UICollectionViewLeftAlignedLayout/include/UICollectionViewLeftAlignedLayout.h
@@ -1,5 +1,4 @@
-
-// Copyright (c) 2014 Giovanni Lodi
+// Copyright (c) 2014-2023 Giovanni Lodi
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy of
 // this software and associated documentation files (the "Software"), to deal in
